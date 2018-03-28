8ad8f973312cbbb97850b26779e514d5ef21b3ad
Daiane Bushey <daianebushey@gmail.com>
2018-03-28 22:37:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 == @side2 && @side1 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  def valid_triangle
+  sides = [@side1, @side2, @side1]
+  raise TriangleError if sides.any? { |side| side == 0 }
+
+  sides.inject(:+) - sides.max > sides.max ? true : false
+  end
+
+  valid_triangle
+
+  class TriangleError < StandardError
+
+  end
 end
